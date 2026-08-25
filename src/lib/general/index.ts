9c1ac48d072bf7c1fc2c57f795dbc808e6888ccc
andygargol2010-afk <andygargol2010@gmail.com>
2026-08-24 21:06:55 -0300

    feat: include education tools in canonical general catalog

--- src/lib/general/index.ts
+++ src/lib/general/index.ts
@@ -1,4 +1,4 @@
 import {GENERAL_CATEGORIES} from "./catalog";
-import {MATH_TOOLS} from "./math";import {TEXT_TOOLS} from "./text";import {DEV_TOOLS} from "./dev";import {CONVERTER_TOOLS} from "./converters";import {TIME_TOOLS} from "./time";import {MISC_TOOLS} from "./misc";import {SCIENCE_TOOLS} from "./science";import {PRODUCTIVITY_TOOLS} from "./productivity";import {MEDIA_TOOLS} from "./media";import {LIFESTYLE_TOOLS} from "./lifestyle";
+import {MATH_TOOLS} from "./math";import {TEXT_TOOLS} from "./text";import {DEV_TOOLS} from "./dev";import {CONVERTER_TOOLS} from "./converters";import {TIME_TOOLS} from "./time";import {MISC_TOOLS} from "./misc";import {SCIENCE_TOOLS} from "./science";import {PRODUCTIVITY_TOOLS} from "./productivity";import {MEDIA_TOOLS} from "./media";import {LIFESTYLE_TOOLS} from "./lifestyle";import {EDUCATION_TOOLS} from "./education";
 export {GENERAL_CATEGORIES};
-export const GENERAL_TOOLS=[...MATH_TOOLS,...TEXT_TOOLS,...DEV_TOOLS,...CONVERTER_TOOLS,...TIME_TOOLS,...MISC_TOOLS,...SCIENCE_TOOLS,...PRODUCTIVITY_TOOLS,...MEDIA_TOOLS,...LIFESTYLE_TOOLS];
+export const GENERAL_TOOLS=[...MATH_TOOLS,...TEXT_TOOLS,...DEV_TOOLS,...CONVERTER_TOOLS,...TIME_TOOLS,...MISC_TOOLS,...SCIENCE_TOOLS,...PRODUCTIVITY_TOOLS,...MEDIA_TOOLS,...LIFESTYLE_TOOLS,...EDUCATION_TOOLS];
