e92cae235316144768052acae5a344fa2bdb45f9
andygargol2010-afk <andygargol2010@gmail.com>
2026-08-25 07:11:04 -0300

    fix: register financial routes in generated route tree

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -6,12 +6,18 @@ import { Route as SitemapDotxmlRouteImport } from './routes/sitemap[.]xml'
 import { Route as CategoriaSlugRouteImport } from './routes/categoria.$slug'
 import { Route as HerramientasIndexRouteImport } from './routes/herramientas.index'
 import { Route as HerramientasSlugRouteImport } from './routes/herramientas.$slug'
+import { Route as FinanzasRouteImport } from './routes/finanzas'
+import { Route as FinanzasSlugRouteImport } from './routes/finanzas.$slug'
+
 const IndexRoute=IndexRouteImport.update({id:'/',path:'/',getParentRoute:()=>rootRouteImport} as any)
 const SitemapDotxmlRoute=SitemapDotxmlRouteImport.update({id:'/sitemap.xml',path:'/sitemap.xml',getParentRoute:()=>rootRouteImport} as any)
 const CategoriaSlugRoute=CategoriaSlugRouteImport.update({id:'/categoria/$slug',path:'/categoria/$slug',getParentRoute:()=>rootRouteImport} as any)
 const HerramientasIndexRoute=HerramientasIndexRouteImport.update({id:'/herramientas/',path:'/herramientas/',getParentRoute:()=>rootRouteImport} as any)
 const HerramientasSlugRoute=HerramientasSlugRouteImport.update({id:'/herramientas/$slug',path:'/herramientas/$slug',getParentRoute:()=>rootRouteImport} as any)
-const rootRouteChildren={IndexRoute,SitemapDotxmlRoute,CategoriaSlugRoute,HerramientasSlugRoute,HerramientasIndexRoute}
+const FinanzasRoute=FinanzasRouteImport.update({id:'/finanzas',path:'/finanzas',getParentRoute:()=>rootRouteImport} as any)
+const FinanzasSlugRoute=FinanzasSlugRouteImport.update({id:'/finanzas/$slug',path:'/finanzas/$slug',getParentRoute:()=>rootRouteImport} as any)
+
+const rootRouteChildren={IndexRoute,SitemapDotxmlRoute,CategoriaSlugRoute,HerramientasSlugRoute,HerramientasIndexRoute,FinanzasRoute,FinanzasSlugRoute}
 export const routeTree=rootRouteImport._addFileChildren(rootRouteChildren)._addFileTypes<any>()
 import type { getRouter } from './router.tsx'
 import type { startInstance } from './start.ts'
