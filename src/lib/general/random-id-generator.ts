bcdaf766c300ee503cc163064e1701d6f292b2ed
andygargol2010-afk <andygargol2010@gmail.com>
2026-08-24 22:52:34 -0300

    chore: remove unused random identifier helper

--- src/lib/general/random-id-generator.ts
+++
@@ -1,6 +0,0 @@
-export function generateRandomId(length=8){
-  const alphabet="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  const values=new Uint32Array(length);
-  crypto.getRandomValues(values);
-  return Array.from(values,v=>alphabet[v%alphabet.length]).join("");
-}
