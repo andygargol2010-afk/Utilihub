79439cf3efb2519659a2b006c426e6b9a3207ed9
andygargol2010-afk <andygargol2010@gmail.com>
2026-08-27 08:08:51 -0300

    feat: register independent calculus bank

--- src/lib/general/education-bank-a/index.ts
+++ src/lib/general/education-bank-a/index.ts
@@ -1,10 +1,12 @@
 import { ARITMETICA } from "./aritmetica";
 import { ALGEBRA } from "./algebra";
 import { GEOMETRIA } from "./geometria";
+import { CALCULO } from "./calculo";
 
 // Only banks fully migrated to independent questions are registered here.
 export const EDUCATION_BANK_A_REAL={
   aritmetica: ARITMETICA,
   algebra: ALGEBRA,
   geometria: GEOMETRIA,
+  calculo: CALCULO,
 };
