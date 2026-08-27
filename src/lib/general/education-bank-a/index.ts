08a80a983813c4eacc421e43cc06836b68b13613
andygargol2010-afk <andygargol2010@gmail.com>
2026-08-27 09:01:45 -0300

    feat: register independent grammar bank

--- src/lib/general/education-bank-a/index.ts
+++ src/lib/general/education-bank-a/index.ts
@@ -3,6 +3,7 @@ import { ALGEBRA } from "./algebra";
 import { GEOMETRIA } from "./geometria";
 import { CALCULO } from "./calculo";
 import { ESTADISTICA } from "./estadistica";
+import { GRAMATICA } from "./gramatica";
 
 // Only banks fully migrated to independent questions are registered here.
 export const EDUCATION_BANK_A_REAL={
@@ -11,4 +12,5 @@ export const EDUCATION_BANK_A_REAL={
   geometria: GEOMETRIA,
   calculo: CALCULO,
   estadistica: ESTADISTICA,
+  gramatica: GRAMATICA,
 };
