29731c82ffbeece27120ae8a50ec98dacd5a6f46
andygargol2010-afk <andygargol2010@gmail.com>
2026-08-26 22:30:02 -0300

    refactor: split bank A into subject modules

---
+++ src/lib/general/education-bank-a/index.ts
@@ -0,0 +1,13 @@
+import { ARITMETICA } from "./aritmetica";
+import { ALGEBRA } from "./algebra";
+import { GEOMETRIA } from "./geometria";
+import { CALCULO } from "./calculo";
+import { ESTADISTICA } from "./estadistica";
+
+export const EDUCATION_BANK_A_REAL={
+  aritmetica: ARITMETICA,
+  algebra: ALGEBRA,
+  geometria: GEOMETRIA,
+  calculo: CALCULO,
+  estadistica: ESTADISTICA,
+};
